3f790267407ad0119f150d01c5abb0f63ccab928
sverweij <sverweij@users.noreply.github.com>
2018-10-21 13:50:15 +0200

    refactor(update): make the update wrapper (with file io & error messages) unit testable and add unit tests for them

--- bin/upem.js
+++ bin/upem.js
@@ -1,39 +1,20 @@
 #!/usr/bin/env node
-const fs = require('fs')
 const path = require('path')
-const up = require('../src')
+const uppity = require('../src/uppity')
 const readFromStream = require('../src/readFromStream')
 
 const PACKAGE_FILE_NAME = path.join(process.cwd(), 'package.json')
 
-try {
-  const lPackageFile = fs.readFileSync(PACKAGE_FILE_NAME)
-  let $package = JSON.parse(lPackageFile)
+readFromStream()
+  .then(pOutdatedObject => {
+    const lResult = uppity(PACKAGE_FILE_NAME, pOutdatedObject)
 
-  readFromStream().then(pInput => {
-    if (pInput.length <= 0) {
-      process.stdout.write(`  Up'em says: Everything seems to be up to date already.\n\n`)
-      return
+    if (lResult.OK) {
+      process.stdout.write(lResult.message)
+    } else {
+      process.stderr.write(lResult.message)
     }
-    const gOutdatedObject = up.filterOutdatedPackages(JSON.parse(pInput), $package)
-
-    if (Object.keys(gOutdatedObject).length <= 0) {
-      process.stdout.write(`  Up'em says: Everything not in 'upem.donotup' seems to be up to date already.\n\n`)
-      return
-    }
-
-    try {
-      fs.writeFileSync(
-        PACKAGE_FILE_NAME,
-        JSON.stringify(up.updateAllDeps($package, gOutdatedObject), null, 2)
-      )
-      process.stdout.write(`  Up'em just updated all dependencies in package.json to latest\n\n`)
-    } catch (e) {
-      process.stderr.write(`  Up'em encountered a hitch when updating package.json:\n${e}\n\n`)
-    }
-  }).catch(pError => {
+  })
+  .catch(pError => {
     process.stderr.write(`  Up'em encountered a hitch when reading outdated information:\n${pError}\n\n`)
   })
-} catch (e) {
-  process.stderr.write(`  Up'em encountered a hitch when reading 'outdated' input:\n${e}\n\n`)
-}
---
+++ test/uppity.spec.js
@@ -0,0 +1,70 @@
+const fs = require('fs')
+const path = require('path')
+const uppity = require('../src/uppity')
+
+describe('#upppity', () => {
+  test('non-existing package.json errors', () => {
+    const lResult = uppity('thisfiledoesnotexist', '')
+    expect(lResult.OK).toEqual(false)
+    expect(lResult.message).toContain(`Up'em encountered a hitch:`)
+  })
+
+  test('empty string dependency JSON yields "nothing to update"', () => {
+    const lResult = uppity(path.join(__dirname, 'package-in.json'), '')
+    expect(lResult.OK).toEqual(true)
+    expect(lResult.message).toContain(`Up'em says: Everything seems to be up to date already.`)
+  })
+
+  test('{} dependency JSON yields "nothing to update"', () => {
+    const lResult = uppity(path.join(__dirname, 'package-in.json'), '{}')
+    expect(lResult.OK).toEqual(true)
+    expect(lResult.message).toContain(`Up'em says: Everything seems to be up to date already.`)
+  })
+
+  test(`read only package.json yields a 'can't update`, () => {
+    const OUTDATED_JSON = `
+    {
+      "lodash.assign": {
+        "current": "4.2.0",
+        "wanted": "4.2.0",
+        "latest": "4.2.1",
+        "location": "node_modules/lodash.assign"
+      }
+    }
+    `
+    const lResult = uppity(path.join(__dirname, 'package-in-readonly.json'), OUTDATED_JSON)
+    expect(lResult.OK).toEqual(false)
+    expect(lResult.message).toContain(`Up'em encountered a hitch when updating package.json:`)
+  })
+
+  test('if upem.donotup encompasses the outdated object yields that in a message', () => {
+    const OUTDATED_JSON = `
+    {
+      "ts-jest": {
+        "current": "1.8.2",
+        "wanted": "1.8.2",
+        "latest": "2.0.0",
+        "location": "node_modules/ts-jest"
+      }
+    }
+    `
+    const lResult = uppity(path.join(__dirname, 'package-in.json'), OUTDATED_JSON)
+    expect(lResult.OK).toEqual(true)
+    expect(lResult.message).toContain(`Up'em says: Everything not in 'upem.donotup' seems to be up to date already.`)
+  })
+
+  test('happy day: dependencies updated with stuff in an outdated.json', () => {
+    const OUTDATED_JSON = fs.readFileSync(path.join(__dirname, 'outdated.json'))
+    const INPUT_FILENAME = path.join(__dirname, 'package-in.json')
+    const OUTPUT_FILENAME = path.join(__dirname, 'tmp_package-out.json')
+    const FIXTURE_FILENAME = path.join(__dirname, 'package-out.json')
+
+    const lResult = uppity(INPUT_FILENAME, OUTDATED_JSON, OUTPUT_FILENAME)
+
+    expect(lResult.OK).toEqual(true)
+    expect(lResult.message).toContain(`Up'em just updated all dependencies in package.json to latest`)
+    expect(JSON.parse(fs.readFileSync(OUTPUT_FILENAME))).toEqual(JSON.parse(fs.readFileSync(FIXTURE_FILENAME)))
+  })
+})
+
+/* global describe, test, expect */
