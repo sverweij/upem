9cd183e2f741f57aaebf26a4550141c789daa38a
sverweij <sverweij@users.noreply.github.com>
2018-10-21 11:12:02 +0200

    bugfix(update): give a different warning when upem.outdated encompases the outdated object

--- bin/upem.js
+++ bin/upem.js
@@ -11,24 +11,29 @@ try {
   let $package = JSON.parse(lPackageFile)
 
   readFromStream().then(pInput => {
-    if (pInput.length > 0) {
-      const gOutdatedObject = up.filterOutdatedPackages(JSON.parse(pInput), $package)
-
-      try {
-        fs.writeFileSync(
-          PACKAGE_FILE_NAME,
-          JSON.stringify(up.updateAllDeps($package, gOutdatedObject), null, 2)
-        )
-        process.stdout.write('  Up\'em just updated all dependencies in package.json to latest\n\n')
-      } catch (e) {
-        process.stderr.write(`  Up'em encountered a hitch when updating package.json:\n${e}\n\n`)
-      }
-    } else {
+    if (pInput.length <= 0) {
       process.stdout.write(`  Up'em says: Everything seems to be up to date already.\n\n`)
+      return
+    }
+    const gOutdatedObject = up.filterOutdatedPackages(JSON.parse(pInput), $package)
+
+    if (Object.keys(gOutdatedObject).length <= 0) {
+      process.stdout.write(`  Up'em says: Everything not in 'upem.donotup' seems to be up to date already.\n\n`)
+      return
+    }
+
+    try {
+      fs.writeFileSync(
+        PACKAGE_FILE_NAME,
+        JSON.stringify(up.updateAllDeps($package, gOutdatedObject), null, 2)
+      )
+      process.stdout.write(`  Up'em just updated all dependencies in package.json to latest\n\n`)
+    } catch (e) {
+      process.stderr.write(`  Up'em encountered a hitch when updating package.json:\n${e}\n\n`)
     }
   }).catch(pError => {
     process.stderr.write(`  Up'em encountered a hitch when reading outdated information:\n${pError}\n\n`)
   })
 } catch (e) {
-  process.stderr.write(`  Up'em encountered a hitch when reading stuff:\n${e}\n\n`)
+  process.stderr.write(`  Up'em encountered a hitch when reading 'outdated' input:\n${e}\n\n`)
 }
