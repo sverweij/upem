bcc4b26b12a507573b85cef7d2c50722c3358a55
sverweij <sverweij@users.noreply.github.com>
2019-04-07 12:13:53 +0200

    build(npm): tweak generated dependency graph

--- src/readFromStream.js
+++ src/readFromStream.js
@@ -1,21 +1,22 @@
 module.exports = function (pInStream = process.stdin) {
   return new Promise((resolve, reject) => {
     let lInput = ''
-    pInStream.resume()
-    pInStream.setEncoding('utf8')
-    pInStream.on('data', (pChunk) => {
-      lInput += pChunk
-    })
-    pInStream.on('end', () => {
-      try {
-        pInStream.pause()
-        resolve(lInput)
-      } catch (e) {
+    pInStream
+      .resume()
+      .setEncoding('utf8')
+      .on('data', (pChunk) => {
+        lInput += pChunk
+      })
+      .on('end', () => {
+        try {
+          pInStream.pause()
+          resolve(lInput)
+        } catch (e) {
+          reject(e)
+        }
+      })
+      .on('error', (e) => {
         reject(e)
-      }
-    })
-    pInStream.on('error', (e) => {
-      reject(e)
-    })
+      })
   })
 }
